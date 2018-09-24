1a037edc2ef56723fa0926b982b31033e262a1d4
NathanaelElbert <nathan.elbert2000@gmail.com>
2018-09-24 16:58:46 -0700

    Rotate Fix

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -4,62 +4,93 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
-    // TODO: also add a function that rotates in mobile
     [SerializeField] Transform rotateParent = null; // optional to rotate a different transform
-    [SerializeField] bool autoRotate = true;
-    [SerializeField] float autoRotateSpeed = 0.5f;
-    [SerializeField] float maxRotation = 10f;
 
-    private bool goOn;
-    private bool rotate;
-
-   
-
+    public float moveSpeed = 10f;
+    public float turnSpeed = 50f;
     Transform objectToRotate;
 
-    void Start()
+
+    private void Start()
     {
         objectToRotate = (rotateParent == null) ? transform : rotateParent;
-        goOn.Equals(false);
-        rotate.Equals(autoRotate);
     }
 
     void Update()
     {
+        if (Input.GetKey(KeyCode.UpArrow))
+            objectToRotate.Rotate(Vector3.left, -turnSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.DownArrow))
+            objectToRotate.Rotate(Vector3.left, turnSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            objectToRotate.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            objectToRotate.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+    }
+}
+
+    
+    
+    
+    // TODO: also add a function that rotates in mobile
+   
+   // [SerializeField] bool autoRotate = true;
+    //[SerializeField] float autoRotateSpeed = 0.5f;
+    //[SerializeField] float maxRotation = 10f;
+
+    //private bool goOn;
+    //private bool rotate;
+
+   
+
+    //Transform objectToRotate;
+
+    //void Start()
+    //{
+      //  objectToRotate = (rotateParent == null) ? transform : rotateParent;
+        //goOn.Equals(false);
+   //     rotate.Equals(autoRotate);
+   // }
+
+   // void Update()
+   // {
         
 
 
-        if (goOn.Equals(true))
-        {
-            rotate = false;
-        }
+     //   if (goOn.Equals(true))
+       // {
+         //   rotate = false;
+        //}
 
-        if (goOn.Equals(false) && autoRotate.Equals(true))
-        {
-            rotate = true;
-        }
+        //if (goOn.Equals(false) && autoRotate.Equals(true))
+        //{
+          //  rotate = true;
+        //}
 
 
 
-        if (rotate.Equals(true))
-        {
-            objectToRotate.rotation = Quaternion.Euler(0f, maxRotation * Mathf.Sin(Time.time * autoRotateSpeed), 0f);
+        //if (rotate.Equals(true))
+        //{
+          //  objectToRotate.rotation = Quaternion.Euler(0f, maxRotation * Mathf.Sin(Time.time * autoRotateSpeed), 0f);
             
-        }
+        //}
 
 
-        if (PlayerMoveController.go.Equals(true))
-        {
-            goOn = true;
-        }
+        //if (PlayerMoveController.go.Equals(true))
+        //{
+          //  goOn = true;
+        //}
 
 
        
 
 
 
-    }
+    //}
 
    
     
-}
+//}
