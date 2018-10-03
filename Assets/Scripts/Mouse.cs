82c664e151609518a3b48c3d23b888cee4a54747
Elbert <W1651754@losrios.edu>
2018-10-03 10:54:35 -0700

    particles 1

---
+++ Assets/Scripts/Mouse.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse : MonoBehaviour {
+
+
+
+     void Update()
+    {
+        x = Input.mousePosition.x / mainCamera.pixelWidth;
+        y = Input.mousePosition.y / mainCamera.pixelHeight;
+        transform.position = Vector3(x, y, 0);
+
+    }
+
+    
+
+
+}
