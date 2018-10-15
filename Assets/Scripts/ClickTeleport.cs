6315374ab771f157917a0f81540cdf3d0859495e
Elbert <W1651754@losrios.edu>
2018-10-15 11:12:19 -0700

    new controls 1

---
+++ Assets/Scripts/ClickTeleport.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickTeleport : MonoBehaviour {
+
+    Transform teleported;
+
+   void Start()
+    {
+        GameObject[] FindGameObjectsWithLayer(int layer)
+        {
+            GameObject[] goArray = FindObjectsOfType(typeof(GameObject)) as GameObject[];
+            List goList = new List();
+            for (int i = 0; i < goArray.Length; i++)
+            {
+                if (goArray[i].layer == layer)
+                {
+                    goList.Add(goArray[i]);
+                }
+            }
+            if (goList.Count == 0)
+            {
+                return null;
+            }
+            return goList.ToArray();
+        }
+    }
+    void OnMouseDown()
+    {
+        if (teleported.gameObject.layer.Equals(1))
+        {
+            
+        }
+    }
+}
+
+
